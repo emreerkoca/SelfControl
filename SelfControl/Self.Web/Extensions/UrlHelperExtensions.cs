bd8bec2ff1771dac57bf665fb9a06fd29af15adc
emreerkoca <emreerkoca@outlook.com>
2019-07-02 23:32:04 +0300

    Adding Identity part 2

---
+++ SelfControl/Self.Web/Extensions/UrlHelperExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Self.Web.Extensions
+{
+    public static class UrlHelperExtensions
+    {
+        public static string EmailConfirmationLink(this IUrlHelper urlHelper, string userId, string code, string scheme)
+        {
+            return urlHelper.Action(
+                action: "GET",
+                controller: "ConfirmEmail",
+                values: new { userId, code },
+                protocol: scheme);
+        }
+    }
+}
