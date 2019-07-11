6f397eca6c35fbc1b907a88999e3907449744b4f
emreerkoca <emreerkoca@outlook.com>
2019-07-12 02:21:24 +0300

    restore nuget packages

--- SelfControl/Self.Infrastructure/Identity/AppIdentityDbContextSeed.cs
+++ SelfControl/Self.Infrastructure/Identity/AppIdentityDbContextSeed.cs
@@ -1,17 +1,14 @@
-﻿using Microsoft.AspNetCore.Identity;
-using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
+
 
 namespace Self.Infrastructure.Identity
 {
     public class AppIdentityDbContextSeed
     {
-        public static async Task SeedAsync(UserManager<ApplicationUser> userManager)
-        {
-            var defaultUser = new ApplicationUser { UserName = "emreerkoca", Email = "emreerkoca@outlook.com" };
-            await userManager.CreateAsync(defaultUser, "123456");
-        }
+        //public static async Task SeedAsync(UserManager<ApplicationUser> userManager)
+        //{
+        //    var defaultUser = new ApplicationUser { UserName = "emreerkoca", Email = "emreerkoca@outlook.com" };
+        //    await userManager.CreateAsync(defaultUser, "123456");
+        //}
     }
 }
