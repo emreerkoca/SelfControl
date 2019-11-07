ad615e651f7f1a66ca841c22133e9d51e52646a3
Emre Erkoca <emreerkoca@outlook.com>
2019-11-08 02:48:59 +0300

    components created

--- SelfControl/Self.WebSpa/ClientApp/src/app/app.module.ts
+++ SelfControl/Self.WebSpa/ClientApp/src/app/app.module.ts
@@ -9,6 +9,9 @@ import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import { AddWordComponent } from './add-word/add-word.component';
+import { UpdateWordComponent } from './update-word/update-word.component';
+import { GetWordsComponent } from './get-words/get-words.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +19,10 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
     NavMenuComponent,
     HomeComponent,
     CounterComponent,
-    FetchDataComponent
+    FetchDataComponent,
+    AddWordComponent,
+    UpdateWordComponent,
+    GetWordsComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
@@ -26,6 +32,10 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'counter', component: CounterComponent },
       { path: 'fetch-data', component: FetchDataComponent },
+      { path: 'add-word', component: AddWordComponent },
+      { path: 'update-word/:id', component: UpdateWordComponent },
+      { path: 'get-words', component: GetWordsComponent },
+      { path: '**', redirectTo: '/' } // for invalid paths
     ])
   ],
   providers: [],
---
+++ SelfControl/Self.WebSpa/ClientApp/src/app/get-words/get-words.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-get-words',
+  templateUrl: './get-words.component.html',
+  styleUrls: ['./get-words.component.css']
+})
+export class GetWordsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
